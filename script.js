7fb014583896d3111976e1b06ca4740442f20c72
XYJgrace <gracexiao26@gmail.com>
2024-12-28 21:26:17 -0800

    update buttons

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 /* .js files add interaction to your website */
+
+/* front page project buttons */
+
+var factList = [
+    "Actuator and servo testing.", /*0*/
+        
+    "Software skills used include C, Python, MATLAB, Fusion 360.",
+  ];
+  
+  var fact = document.getElementById("fact");
+  var factButton = document.getElementById("factButton");
+  var count = 0;
+  
+  if (factButton) {
+    factButton.addEventListener("click", displayFact);
+  }
+  
+  function displayFact() {
+    fact.innerHTML = factList[count];
+    count ++;
+    if(count == factList.length) {
+      count=0;
+    }
+  }
