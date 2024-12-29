e1192b38988029f19774dc73d5c6786f60f602c6
XYJgrace <gracexiao26@gmail.com>
2024-12-29 15:20:24 -0800

    update styling

--- script.js
+++ script.js
@@ -1,29 +1,4 @@
-/* .js files add interaction to your website */
-
 /* front page project buttons */
-
-/*
-var factList = [
-    "As part of UCSD SEDS. Performed actuator and servo testing, CAD modeling; Software skills used include C, Python, MATLAB, Fusion 360.", 
-        
-    ".",
-  ];
-  
-  var fact = document.getElementById("fact");
-  var factButton = document.getElementById("factButton");
-  var count = 0;
-  
-  if (factButton) {
-    factButton.addEventListener("click", displayFact);
-  }
-  
-  function displayFact() {
-    fact.innerHTML = factList[count];
-    count ++;
-    if(count == factList.length) {
-      count=0;
-    }
-  }*/
 
 // Define fact lists for each button
 var factLists = [
